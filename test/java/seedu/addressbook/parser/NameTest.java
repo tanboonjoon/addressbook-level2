edf0734a0fa0bccff38b257e758f918169b3c524
tanboonjoon <tbj_999@hotmail.com>
2016-09-09 00:15:44 +0800

    add test case to test same name

--- test/java/seedu/addressbook/parser/NameTest.java
+++ test/java/seedu/addressbook/parser/NameTest.java
@@ -2,13 +2,36 @@ package seedu.addressbook.parser;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class NameTest {
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
 
+public class NameTest {
+	Name name1;
+	Name name2;
+	Name name3;
+	Name name4;
+	Name nameNull;
+
+	@Before
+	public void setUp() throws IllegalValueException {
+		name1 = new Name("Tan");
+		name2 = new Name("Alex");
+		name3 = new Name("tAN");
+		name4 = new Name("BOON jOon");
+			
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isSimilar_SameName() {
+		assertTrue(name1.isSimilar(name3));
+	}
 	}
 
 }
