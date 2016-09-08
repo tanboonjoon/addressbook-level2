0f0e2e4d48c0a2e44e4d88dac798b92a4e366c23
tanboonjoon <tbj_999@hotmail.com>
2016-09-09 00:11:45 +0800

    set up variables for testing

---
+++ test/java/seedu/addressbook/parser/NameTest.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
