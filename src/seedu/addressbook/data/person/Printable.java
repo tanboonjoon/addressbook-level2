74c066a44d0dfcbd27719c02210dffe94d286558
tanboonjoon <tbj_999@hotmail.com>
2016-09-11 11:30:53 +0800

    use 4 space instead of tab

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,7 +1,12 @@
 package seedu.addressbook.data.person;
-
+/*
+ * Printable interface for person
+ * 
+ */
 public interface Printable {
-	
-	public String getPrintableString();
+
+
+
+    public String getPrintableString();
 
 }
